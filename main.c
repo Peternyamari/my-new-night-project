2669696110b46e78492352909bb812eec49b6b47
Peternyamari <113664817+Peternyamari@users.noreply.github.com>
2022-09-23 00:42:50 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+
+    int age = 28;
+    int meanscore = 9;
+    char admission = 'D';
+    char subject[] = "My name is Peter Nyamari";
+    printf("Peter\nMogeni\nNyamari\n");
+    printf("   /|\n");
+    printf("  / |\n");
+    printf(" /  |\n");
+    printf("/___|\n");
+    printf("%s\n", subject);
+    printf("i am %d years old.\n", age);
+    printf("i got a meanscore of %d\n",meanscore);
+
+
+    return 0;
+}
